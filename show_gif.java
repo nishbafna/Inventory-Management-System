6ba648b29225292ea5aa2295bdf57c2080a3cbaf
nishbafna <31662663+nishbafna@users.noreply.github.com>
2018-02-21 23:12:52 +0530

    Add files via upload

---
+++ show_gif.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Naman Doshi
+ */
+import java.io.*;
+import javax.swing.*;
+public class show_gif extends javax.swing.JFrame {
+
+  public static void main(String args[])
+  {System.out.println("fuck off"); 
+}
+}
